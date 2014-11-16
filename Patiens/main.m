a23ec1e587199ab65440d17684ca1acda96aca07
Juan Ml. Morillo <juanmorillios@gmail.com>
2014-11-16 20:14:45 +0100

    Initial Commit

---
+++ Patiens/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Patiens
+//
+//  Created by Juan Morillo on 16/11/14.
+//  Copyright (c) 2014 JuanMorillios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
